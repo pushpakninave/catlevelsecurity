ab312da2f61303df66ae2ba65e4d0f2b2b554efb
pushpakninave <pushpakninave.jee@gmail.com>
2024-12-25 18:06:54 +0530

    default form login setup

---
+++ src/main/java/com/crm/secure/securityConfig/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.crm.secure.securityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/login", "/register")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
